bfbe3759591804eb08b24f7e9537bb3812490ee4
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-04-07 12:40:03 +0200

    Connecting to photon with player name

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("Connection Status")]
+    [SerializeField] private Text _connectionStatusText;
+
+    [Header("Login UI Panel")]
+    [SerializeField] private InputField _playerNameInput;
+
+    #region Unity Methods
+
+    private void Update()
+    {
+        SetConnectionStatusText();
+    }
+
+    private void SetConnectionStatusText()
+    {
+        _connectionStatusText.text = "Connection status: " + PhotonNetwork.NetworkClientState;
+    }
+
+    #endregion
+
+    #region UI Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = _playerNameInput.text;
+
+        if(string.IsNullOrEmpty(playerName))
+        {
+            Debug.Log("Player name is invalid!");
+            return;
+        }
+
+        PhotonNetwork.LocalPlayer.NickName = playerName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet.");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon.");
+    }
+
+    #endregion
+}
