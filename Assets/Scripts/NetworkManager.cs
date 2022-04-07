38741b4e39bdce40f5d8dac571a4cc74d138e4ee
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-04-07 16:36:09 +0200

    Added showing existing rooms in a list

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
@@ -25,9 +26,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     [Header("Room List UI Panel")]
     [SerializeField] private GameObject _roomListUIPanel;
+    [SerializeField] private GameObject _roomListEntryPrefab;
+    [SerializeField] private GameObject _roomListParentGameObject;
 
     [Header("Join Random Room UI Panel")]
     [SerializeField] private GameObject _joinRandomRoomUIPanel;
+
+    private Dictionary<string, RoomInfo> _cachedRoomList = new Dictionary<string, RoomInfo>();
+    private Dictionary<string, GameObject> _roomListGameObjects = new Dictionary<string, GameObject>();
 
     #region Unity Methods
 
@@ -56,6 +62,26 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         _joinRandomRoomUIPanel.SetActive(panelToBeActivated.Equals(_joinRandomRoomUIPanel.name));
     }
 
+    private void OnJoinRoomButtonClicked(string roomName)
+    {
+        if(PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    private void ClearRoomListView()
+    {
+        foreach (GameObject roomListGameObject in _roomListGameObjects.Values)
+        {
+            Destroy(roomListGameObject);
+        }
+
+        _roomListGameObjects.Clear();
+    }
+
     #endregion
 
     #region UI Callbacks
@@ -78,13 +104,15 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         string roomName = _roomNameInputField.text;
 
-        if(string.IsNullOrEmpty(roomName) || string.IsNullOrEmpty(_maxPlayersInputField.text))
+        if(string.IsNullOrEmpty(roomName))
         {
-            Debug.Log("Room name or max number of players is invalid!");
-            return;
+            roomName = "Room " + Random.Range(1000, 10000);
         }
 
-        roomName = "Room " + Random.Range(1000, 10000);
+        if(string.IsNullOrEmpty(_maxPlayersInputField.text))
+        {
+            _maxPlayersInputField.text = "3";
+        }
 
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = (byte)int.Parse(_maxPlayersInputField.text);
@@ -94,6 +122,26 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void OnCancelButtonClicked()
     {
+        ActivatePanel(_gameOptionsUIPanel.name);
+    }
+
+    public void OnShowRoomListButtonClicked()
+    {
+        if(!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        ActivatePanel(_roomListUIPanel.name);
+    }
+
+    public void OnBackButtonClicked()
+    {
+        if(PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
         ActivatePanel(_gameOptionsUIPanel.name);
     }
 
@@ -121,6 +169,55 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
         ActivatePanel(_insideRoomUIPanel.name);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        ClearRoomListView();
+
+        foreach (RoomInfo room in roomList)
+        {
+            Debug.Log(room.Name);
+
+            if(!room.IsOpen || !room.IsVisible || room.RemovedFromList)
+            {
+                if(_cachedRoomList.ContainsKey(room.Name))
+                {
+                    _cachedRoomList.Remove(room.Name);
+                }
+
+                return;
+            }
+
+            //Update cachedRoom list
+            if(_cachedRoomList.ContainsKey(room.Name))
+            {
+                _cachedRoomList[room.Name] = room;
+                return;
+            }
+
+            //Add the new room to the cachedRoom list
+            _cachedRoomList.Add(room.Name, room);
+        }
+
+        foreach(RoomInfo room in _cachedRoomList.Values)
+        {
+            GameObject roomListEntryGameObject = Instantiate(_roomListEntryPrefab);
+            roomListEntryGameObject.transform.SetParent(_roomListParentGameObject.transform);
+            roomListEntryGameObject.transform.localScale = Vector3.one;
+
+            roomListEntryGameObject.transform.Find("RoomNameText").GetComponent<Text>().text = room.Name;
+            roomListEntryGameObject.transform.Find("RoomPlayersText").GetComponent<Text>().text = room.PlayerCount + " / " + room.MaxPlayers;
+            roomListEntryGameObject.transform.Find("JoinRoomButton").GetComponent<Button>().onClick.AddListener(() => OnJoinRoomButtonClicked(room.Name));
+
+            _roomListGameObjects.Add(room.Name, roomListEntryGameObject);
+        }
+    }
+
+    public override void OnLeftLobby()
+    {
+        ClearRoomListView();
+        _cachedRoomList.Clear();
     }
 
     #endregion
