5c2138456087f35315a357697c6c7057351255ab
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-05-09 16:35:13 +0200

    Added spawning players

---
+++ Assets/Scripts/MobileFPSGameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class MobileFPSGameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+
+    private void Start()
+    {
+        if(PhotonNetwork.IsConnectedAndReady)
+        {
+            SpawnPlayer();
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        if(_playerPrefab != null)
+        {
+            int randomSpawnPoint = Random.Range(-10, 10);
+            PhotonNetwork.Instantiate(_playerPrefab.name, new Vector3(randomSpawnPoint, 0f, randomSpawnPoint), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _fpsHands;
+    [SerializeField] private GameObject _soldier;
+
+    private void Start()
+    {
+        SoldierBodyAcitvity();
+    }
+
+    private void SoldierBodyAcitvity()
+    {
+        if (photonView.IsMine)
+        {
+            //Activate FPS Hands, Deactivate Soldier
+            _fpsHands.SetActive(true);
+            _soldier.SetActive(false);
+            return;
+        }
+
+        //Activate Soldier, Deactivate FPS Hands
+        _fpsHands.SetActive(false);
+        _soldier.SetActive(true);
+    }
+}
