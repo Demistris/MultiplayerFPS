b4b3b4b3a66d85b2c89db2da3d537bb61f239293
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-05-10 16:01:57 +0200

    Killing mechanism WIP

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class Shooting : MonoBehaviour
+public class Shooting : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private GameObject _hitEffectPrefab;
+
+    [Header("Health")]
+    [SerializeField] private float _startHealth = 100f;
+    [SerializeField] private Image _healthBar;
+
+    private float _heatlh;
+
+    private void Start()
+    {
+        _heatlh = _startHealth;
+        _healthBar.fillAmount = _heatlh / _startHealth;
+    }
 
     public void Fire()
     {
@@ -12,9 +26,30 @@ public class Shooting : MonoBehaviour
 
         if(Physics.Raycast(ray, out hit, 100f))
         {
-            //Debug.Log("Hit " + hit.collider.gameObject.name);
             GameObject hitEffect = Instantiate(_hitEffectPrefab, hit.point, Quaternion.identity);
             Destroy(hitEffect, 0.5f);
+
+            GameObject hitObject = hit.collider.gameObject;
+            PhotonView photonView = hitObject.GetComponent<PhotonView>();
+
+            if (hitObject.CompareTag("Player") && !photonView.IsMine)
+            {
+                photonView.RPC("TakeDamage", RpcTarget.AllBuffered, 10f);
+            }
+        }
+    }
+
+    [PunRPC]
+    private void TakeDamage(float damage, PhotonMessageInfo info)
+    {
+        _heatlh -= damage;
+        _healthBar.fillAmount = _heatlh / _startHealth;
+        Debug.Log(_heatlh);
+
+        if(_heatlh <= 0f)
+        {
+            //Die();
+            Debug.Log(info.Sender.NickName + " killed " + info.photonView.Owner.NickName);
         }
     }
 }
