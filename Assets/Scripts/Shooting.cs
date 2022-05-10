543f152893bc0b7d495ba6c35eb9c413d4d5a92c
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-05-10 15:39:46 +0200

    Added shooting with hit effects

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _hitEffectPrefab;
+
+    public void Fire()
+    {
+        RaycastHit hit;
+        Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+
+        if(Physics.Raycast(ray, out hit, 100f))
+        {
+            //Debug.Log("Hit " + hit.collider.gameObject.name);
+            GameObject hitEffect = Instantiate(_hitEffectPrefab, hit.point, Quaternion.identity);
+            Destroy(hitEffect, 0.5f);
+        }
+    }
+}
